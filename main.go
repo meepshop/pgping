51ee37d9ac8f9719ee6044e9991ab0e0ceb4f4fd
nukr <nukrs.w@gmail.com>
2018-09-02 11:45:05 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/cenkalti/backoff"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connectionString := os.Args[1]
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	backoff.Retry(func() error {
+		err := db.Ping()
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		return nil
+	}, backoff.NewExponentialBackOff())
+}
